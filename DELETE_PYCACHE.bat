473530417e89a16b2fad157708c0a2e5d1918d0c
1Lucifer12345 <166340540+1Lucifer12345@users.noreply.github.com>
2024-04-11 23:48:29 +0200

    Delete DELETE_PYCACHE.bat

--- DELETE_PYCACHE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rmdir /s /q __pycache__
-rmdir /s /q TwitchChannelPointsMiner\__pycache__
-rmdir /s /q TwitchChannelPointsMiner\classes\__pycache__
-rmdir /s /q TwitchChannelPointsMiner\classes\entities\__pycache__
