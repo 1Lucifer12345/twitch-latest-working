f5a0758616ad0cdab1640e3b137d266b7cb5dbc9
1Lucifer12345 <166340540+1Lucifer12345@users.noreply.github.com>
2024-07-19 04:33:25 +0300

    Update mainsetup.py

--- mainsetup.py
+++ mainsetup.py
@@ -21,10 +21,11 @@ twitch_miner = TwitchChannelPointsMiner(
     username="1lucifer1234",
     password="",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=False,                  # If you want to auto claim all drops from Twitch inventory on the startup
-    priority=[                                  # Custom priority in this case for example:
+    priority=[       
+        Priority.ORDER                          # Custom priority in this case for example:
         Priority.STREAK,                        # - We want first of all to catch all watch streak from all streamers
         Priority.DROPS,                         # - When we don't have anymore watch streak to catch, wait until all drops are collected over the streamers
-        Priority.ORDER                          # - When we have all of the drops claimed and no watch-streak available, use the order priority (POINTS_ASCENDING, POINTS_DESCEDING)
+                                  # - When we have all of the drops claimed and no watch-streak available, use the order priority (POINTS_ASCENDING, POINTS_DESCEDING)
     ],
     enable_analytics=False,                     # Disables Analytics if False. Disabling it significantly reduces memory consumption
     disable_ssl_cert_verification=False,        # Set to True at your own risk and only to fix SSL: CERTIFICATE_VERIFY_FAILED error
